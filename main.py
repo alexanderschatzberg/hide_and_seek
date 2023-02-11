ed539a025e9f2c166545429c99f61c44531a1d3e
Alexander Schatzberg <alex1nderschatzberg@gmail.com>
2023-02-10 22:47:20 -0800

    Start over with the arcade package

--- main.py
+++ main.py
@@ -1,76 +1,45 @@
-from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.properties import (
-    NumericProperty, ReferenceListProperty, ObjectProperty
-)
-from kivy.core.window import Window
-from kivy.vector import Vector
-from kivy.clock import Clock
+import arcade 
+
+"""
+Platformer Game
+"""
+import arcade
+
+# Constants
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 650
+SCREEN_TITLE = "Platformer"
 
 
-class PongPaddle(Widget):
-    score = NumericProperty(0)
+class MyGame(arcade.Window):
+    """
+    Main application class.
+    """
 
-    def bounce_ball(self, ball):
-        if self.collide_widget(ball):
-            vx, vy = ball.velocity
-            offset = (ball.center_y - self.center_y) / (self.height / 2)
-            bounced = Vector(-1 * vx, vy)
-            vel = bounced * 1.1
-            ball.velocity = vel.x, vel.y + offset
+    def __init__(self):
 
+        # Call the parent class and set up the window
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
-class PongBall(Widget):
-    velocity_x = NumericProperty(0)
-    velocity_y = NumericProperty(0)
-    velocity = ReferenceListProperty(velocity_x, velocity_y)
+        arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
 
-    def move(self):
-        self.pos = Vector(*self.velocity) + self.pos
+    def setup(self):
+        """Set up the game here. Call this function to restart the game."""
+        pass
 
+    def on_draw(self):
+        """Render the screen."""
 
-class PongGame(Widget):
-    ball = ObjectProperty(None)
-    player1 = ObjectProperty(None)
-    player2 = ObjectProperty(None)
-
-    def serve_ball(self, vel=(4, 0)):
-        self.ball.center = self.center
-        self.ball.velocity = vel
-
-    def update(self, dt):
-        self.ball.move()
-
-        # bounce of paddles
-        self.player1.bounce_ball(self.ball)
-        self.player2.bounce_ball(self.ball)
-
-        # bounce ball off bottom or top
-        if (self.ball.y < self.y) or (self.ball.top > self.top):
-            self.ball.velocity_y *= -1
-
-        # went of to a side to score point?
-        if self.ball.x < self.x:
-            self.player2.score += 1
-            self.serve_ball(vel=(4, 0))
-        if self.ball.right > self.width:
-            self.player1.score += 1
-            self.serve_ball(vel=(-4, 0))
-
-    def on_touch_move(self, touch):
-        if touch.x < self.width / 3:
-            self.player1.center_y = touch.y
-        if touch.x > self.width - self.width / 3:
-            self.player2.center_y = touch.y
+        self.clear()
+        # Code to draw the screen goes here
 
 
-class PongApp(App):
-    def build(self):
-        game = PongGame()
-        game.serve_ball()
-        Clock.schedule_interval(game.update, 1.0 / 60.0)
-        return game
+def main():
+    """Main function"""
+    window = MyGame()
+    window.setup()
+    arcade.run()
 
 
-if __name__ == '__main__':
-    PongApp().run()
+if __name__ == "__main__":
+    main()
